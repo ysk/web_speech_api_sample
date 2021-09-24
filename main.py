159715c4ca9221a6a70e6c3808fffdbaae6580da
YusukeIkeda <yusuke_aono@yahoo.co.jp>
2021-09-24 19:32:22 +0900

    googleのAPIは一旦使わない

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
-import speech_recognition as sr
+# import speech_recognition as sr
 
-def Learning():
-    r = sr.Recognizer()  
-    cnt = 0
-    list = []
-    with sr.Microphone() as source:
-        while(cnt<4):  #ここの数を増やせば、よりタフネスが上がります
-            r.adjust_for_ambient_noise(source)
-            print("=== 何か、話しかけてください ===")
-            audio = r.listen(source)
-            print("[o] ===> オーディオGET")
-            print("=== CMU Sphinx音声解析中 ===")
-            text = r.recognize_sphinx(audio)
-            print("You said : " + text)
-            list.append(text)
-            cnt = cnt + 1
-    print(list)
+# def Learning():
+#     r = sr.Recognizer()  
+#     cnt = 0
+#     list = []
+#     with sr.Microphone() as source:
+#         while(cnt<4):  #ここの数を増やせば、よりタフネスが上がります
+#             r.adjust_for_ambient_noise(source)
+#             print("=== 何か、話しかけてください ===")
+#             audio = r.listen(source)
+#             print("[o] ===> オーディオGET")
+#             print("=== CMU Sphinx音声解析中 ===")
+#             text = r.recognize_sphinx(audio)
+#             print("You said : " + text)
+#             list.append(text)
+#             cnt = cnt + 1
+#     print(list)
     
-if __name__ == "__main__":
-    Learning()
+# if __name__ == "__main__":
+#     Learning()
